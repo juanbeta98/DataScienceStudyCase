3a0a2db14b556a8d5b9394de393ea0c9ac994454
juanbeta98 <juan.beta98@gmail.com>
2023-09-30 07:53:38 -0500

    Latest version. Send version.

--- source.py
+++ source.py
@@ -9,6 +9,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report,make_scorer
+from sklearn.svm import SVC
+from sklearn.model_selection import GridSearchCV
+
+
 ################ Upload Data ################
 AuditHistory = pd.read_csv('./Data/AuditHistory.csv')                   # 7690 entries
 SupplierPerformance = pd.read_csv('./Data/SupplierPerformance.csv',index_col='SupplierId')     # 5823 entries
@@ -17,6 +27,7 @@ Suppliers = list(AuditHistory['SupplierId'].unique())               # 818 Suppli
 Derivatives = list(AuditHistory['DerivativeName'].unique())         # 75 Derivatives
 ProductionLines = list(AuditHistory['ProductionLine'].unique())     # 4 Production Lines
 Regions = list(AuditHistory['DerivativeRegion'].unique())           # 6 Regions
+
 
 
 ################ Ploting functions ################
@@ -129,17 +140,21 @@ def generate_temporal_WDandBL(AuditHistory,SupplierPerformance):
 
     return WrongDeliverisInTrim,BacklogsInTrim
 
-def generate_chronological_features(AuditHistory,SupplierPerformance,T):
-    LatestQualification,TimeWithoutChange,LastAudit = list(),list(),list()
+def generate_chronological_features(AuditHistory,SupplierPerformance):
+    LatestQualification,TimeWithoutChange,LastAudit = {t:[] for t in [1,2,3]},list(),list()
     for i in AuditHistory.index:
-        last_audit_date,time_since_last_audit,last_qualification = time_until_last_audit(AuditHistory['SupplierId'][i],AuditHistory['DerivativeName'][i],AuditHistory['RecentMonth'][i],T)
-        if last_audit_date != pd.NaT:
-            LatestQualification.append(last_qualification)
-            LastAudit.append(time_since_last_audit) 
-    
+        last_audit_date,time_since_last_audit,last_qualification = time_until_last_audit(AuditHistory['SupplierId'][i],AuditHistory['DerivativeName'][i],AuditHistory['RecentMonth'][i])
+        for t in [1,2,3]:
+            if not pd.isna(last_audit_date):
+                # print(LatestQualification[t])
+                LatestQualification[t].append(last_qualification[t])
+                LastAudit.append(time_since_last_audit) 
+            else:
+                LatestQualification[t].append(pd.NA)
+                LastAudit.append(pd.NA) 
     return LatestQualification,LastAudit
 
-def time_until_last_audit(supplier, derivative, recent_month,T):
+def time_until_last_audit(supplier, derivative, recent_month):
     # Convert 'RecentMonth' to datetime format
     recent_month = pd.to_datetime(recent_month)
 
@@ -160,10 +175,109 @@ def time_until_last_audit(supplier, derivative, recent_month,T):
             if pd.to_datetime(supplier_data['RecentMonth'][i]) > last_audit_date:
                 last_audit_date = pd.to_datetime(supplier_data['RecentMonth'][i])
                 time_since_last_audit = recent_month - last_audit_date
-                last_qualification = supplier_data[f'T{T}Color'][i]
+                last_qualification = {T:supplier_data[f'T{T}Color'][i] for T in [1,2,3]}
                 
         return last_audit_date,time_since_last_audit,last_qualification
 
     # If no past data, return a default value (you can adjust this based on your needs)
     return pd.NaT,pd.NaT,pd.NaT # pd.NaT represents a missing or undefined datetime value
 
+def clean_observations(x,y):
+    rows_with_missing_x = x[x.isnull().any(axis=1)]
+
+    # Drop rows with missing values from 'x'
+    x_cleaned = x.dropna()
+
+    # Drop corresponding rows from 'y' based on 'rows_with_missing_x' index
+    y_cleaned = y.loc[x_cleaned.index]
+    return x_cleaned,y_cleaned
+
+
+def evaluate_models(X,y):
+    # List of models to evaluate
+    models = [
+        ('RandomForest', RandomForestClassifier()),
+        ('LogisticRegression', LogisticRegression()),
+        ('SVM', SVC())
+    ]
+
+    # Evaluate each model using cross-validation
+    for model_name, model in models:
+        scores = cross_val_score(model, X, y, cv=5)  # 5-fold cross-validation
+        print(f'{model_name}: Mean Accuracy = {round(scores.mean(),5)}, Standard Deviation = {round(scores.std(),5)}')
+    
+
+
+def tuneRandomForest(x,y):
+    # Define the parameter grid for Random Forest
+    param_grid_rf = {
+        'n_estimators': [50, 100, 200],
+        'max_depth': [None, 10, 20],
+        'min_samples_split': [2, 5, 10],
+        'min_samples_leaf': [1, 2, 4]
+    }
+
+    # Create a Random Forest Classifier
+    rf_classifier = RandomForestClassifier(random_state=42)
+
+    # Define the scoring metric (use appropriate scoring metric for your task)
+    scorer = make_scorer(accuracy_score)
+
+    # Perform Grid Search to find the best parameters
+    grid_search_rf = GridSearchCV(rf_classifier, param_grid_rf, scoring=scorer, cv=5)
+    grid_search_rf.fit(x, y)  # Assuming 'T1' is the target variable for T1 stage
+
+    # Print the best parameters
+    print("Best Parameters for Random Forest:", grid_search_rf.best_params_)
+    print("Achieved Accuracy on Validation Set:", grid_search_rf.best_score_)
+
+    model = RandomForestClassifier(**grid_search_rf.best_params_)
+    model.fit(x,y)
+    return model
+
+
+def tuneLogisticRegression(x,y):
+    # Define the parameter grid for Logistic Regression
+    param_grid_lr = {
+        'C': [0.001, 0.01, 0.1, 1, 10, 100],
+        'penalty': ['l1', 'l2']
+    }
+
+    # Create a Logistic Regression Classifier
+    lr_classifier = LogisticRegression(max_iter=1000, random_state=42)
+    # Perform Grid Search to find the best parameters
+    scorer = make_scorer(accuracy_score)
+
+    # Perform Grid Search to find the best parameters
+    grid_search_lr = GridSearchCV(lr_classifier, param_grid_lr, scoring=scorer, cv=5)
+    grid_search_lr.fit(x,y)
+
+    # Define the parameter grid for Logistic Regression
+    param_grid_lr = {
+        'C': [0.001, 0.01, 0.1, 1, 10, 100],
+        'penalty': ['l1', 'l2']
+    }
+
+    # Create a Logistic Regression Classifier
+    lr_classifier = LogisticRegression(max_iter=1000, random_state=42)
+
+    
+    grid_search_lr = GridSearchCV(lr_classifier, param_grid_lr, scoring=scorer, cv=5)
+    grid_search_lr.fit(x,y)
+
+    print("Best Parameters for Logistic Regression:", grid_search_lr.best_params_)
+    print("Achieved Accuracy on Validation Set:", grid_search_lr.best_score_)
+
+    print(grid_search_lr.best_params_)
+    model = LogisticRegression(penalty=grid_search_lr.best_params_['penalty'],C=grid_search_lr.best_params_['C'])
+    model.fit(x,y)
+    return model 
+
+
+def PredictResult(q1,q2,q3):
+    if q3 != 0:
+        return q3
+    elif q2 != 0:
+        return q2
+    else:
+        return q1
